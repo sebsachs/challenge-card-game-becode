9a9319524856a3ff987d94cbc7d0322aaba5386e
Sebastian Sachs <sebastian.sachs1@gmail.com>
2022-02-22 09:15:44 +0100

    commit

---
+++ utils/player.py
@@ -0,0 +1,15 @@
+
+
+from card.py import Card
+
+class Player():
+    def __init__(self,turn_count = 0,number_of_cards = 0):
+        self.number_of_cards = number_of_cards
+        self.turn_count = turn_count
+    def play(self):
+
+    cards = []
+    turn_count = 0
+    number_of_cards = 0
+    history = []
+
